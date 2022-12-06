9bded58be76ce8a892825b750d472f5da7914f40
Julian Gomez <julgomez14@gmail.com>
2022-12-06 12:16:39 -0500

    new settings

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import configExpress from './config/express';
+import configDb from './config/database';
+import routes from './routes';
+
+
 
 dotenv.config();
-
+const port = process.env.PORT || 8080
 const app = express();
+configExpress(app);
+configDb();
+routes(app);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
