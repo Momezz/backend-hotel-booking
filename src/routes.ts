492358d4e055d135d227d768768f2bedf3cf0c9f
Julian Gomez <julgomez14@gmail.com>
2022-12-14 15:45:14 -0500

    we add payment gateway

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import booking from './api/booking';
 import hotel from './api/hotel'
 import post from "./api/post";
 import user from './api/user';
+import payment from './api/payment';
 
 function routes(app: Application):void{
   app.use('/api/bookings', booking);
   app.use('/api/hotels', hotel)
   app.use('/api/posts', post);
   app.use('/api/user', user);
+  app.use('/api/payments', payment);
 }
 
 export default routes;
