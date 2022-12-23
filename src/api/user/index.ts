a8d4fe24ff878f5b68380dde2c25023f9d3476e8
Juan Camilo Orjuela <orjuela9@gmail.com>
2022-12-22 19:40:28 -0500

    set basic login structure

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,13 +1,24 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { handleGetUsers, handleGetUser, handleCreateUser, handleUpdateUser, handleDeleteUser} from './user.controller';
+import {
+  handleGetUsers,
+  handleGetUser,
+  handleCreateUser,
+  handleUpdateUser,
+  handleDeleteUser,
+  handleLoginUser,
+} from "./user.controller";
 
 const router = Router();
 
-router.get('/', handleGetUsers);
-router.get('/:id', handleGetUser);
-router.post('/', handleCreateUser);
-router.patch('/:id', handleUpdateUser);
-router.delete('/:id', handleDeleteUser);
+router.get("/", handleGetUsers);
+router.get("/:id", handleGetUser);
+router.post("/", handleCreateUser);
+router.patch("/:id", handleUpdateUser);
+router.delete("/:id", handleDeleteUser);
+
+//LOGIN
+//POST/api/users/login
+router.post("/login", handleLoginUser);
 
 export default router;
